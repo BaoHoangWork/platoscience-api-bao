d33351a795fb30b32b91c99253bf2c22aea9e4cc
VinhD1nk <vinhduaws@gmail.com>
2025-08-06 17:31:00 +0700

    implement end treatment feature and update assessment serializer

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from apps.assessments.views.assessment_view import AssessmentView
 from apps.users.views.user_view import UserView, LoginView, LogoutView, UpdateUserPasswordView
 from apps.users.views.email_view import VerifyEmailView, ResendVerificationView, ForgotPasswordView, VerifyForgotPasswordCodeView
-from apps.assessments.views.assessment_view import AssessmentView, LatestAssessmentView, CheckTimeIntervalView
+from apps.assessments.views.assessment_view import AssessmentView, LatestAssessmentView, CheckTimeIntervalView, AssessmentStopView
 from apps.assessments.views.question_view import QuestionView
 
 api_patterns = [
@@ -36,6 +36,7 @@ api_patterns = [
     path('assessments/latest/', LatestAssessmentView.as_view(), name='latest-assessment'),
     path('check-time-interval/', CheckTimeIntervalView.as_view(), name='check-time-interval'),
     path('questions/', QuestionView.as_view(), name='question'),
+    path('stop-assessment/', AssessmentStopView.as_view(), name='check-stop-assessments')
 ]
 
 urlpatterns = [
